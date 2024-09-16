b00b583c38421c40da14c6207c613158fa0df6d9
Yann Staggl <76598692+ystaggl@users.noreply.github.com>
2024-09-16 17:35:37 +0200

    Added comments
    added comments explaining some of the code because its hard to understand. Rewriting the code wouldn't be worthwhile as it's obsolete.

--- PipPy.py
+++ PipPy.py
@@ -1,6 +1,12 @@
 """
 Module for creating floating window (Picture-in-Picture) players in python.
-You can set the player by calling the set_player function, e.g. PipPy.set_player("mpv"), by default it uses mpv
+You can set the player by calling the set_player function, e.g. PipPy.set_player("mpv"), by default it uses mpv.
+
+This project is deprecated. It was one of the first of my old scripts I remade in python, and hasn't been touched in a long time. The code is not great.
+Looking at it really makes me want to rewrite it, but it's functionality is better produced with simple mpv config changes,
+as mpv already has most of this functionality built in.
+
+Instead, I thought it'd be fun to add some annotated comments. So if anyone's reading this, hi.
 """
 import os
 import ctypes
@@ -15,24 +21,33 @@ from multiprocessing.connection import Client
 from win32api import GetKeyState
 from win32con import VK_SCROLL
 
+#Why am I using global variables as config options?
 require_scroll = True
-use_logitech_lighting = True
+use_logitech_lighting = True #This relates to LogiPy, a project I worked on simultaneously.
 
-user32 = ctypes.windll.user32
+#These aren't even config options but toggles changed by functions in the script. This is not good.
+window_handle = None
+size_box = False #Toggle if the window is resizeable
+autoplay = False
+mpv_error = False #Tracks if there's been an error I guess? This is not good error handling.
+
+user32 = ctypes.windll.user32 #for setting window styles (i.e. borderless)
+
 #Global Variable Defaults 
 player_class = "mpv"
-if  user32.GetSystemMetrics(0) == 1920:
+#This was meant to set differnt positions depending on if the monitor was vertical, but it requires that you're using 1920x1080 or 1080x1920. This was bad code.
+if  user32.GetSystemMetrics(0) == 1080:
+    (floating_posx,floating_posy,floating_width,floating_height) = (594,1310,323,565)
+else:
     (floating_posx,floating_posy,floating_width,floating_height) = (1310,594,565,323)
     (default_posx,default_posy,default_width,default_height) = (312,125,1296,759)
-elif  user32.GetSystemMetrics(0) == 1080:
-    (floating_posx,floating_posy,floating_width,floating_height) = (594,1310,323,565)
-window_handle = None
-size_box = False
-autoplay = False
-mpv_error = False
+
+
+
 
 #Functions to change defaults.
 #TODO: Change these functions so they create ini files
+# ^ Good idea, I should've done it before this project became obsolete.
 
 
 def set_player(new_player_class):
@@ -75,18 +90,19 @@ def set_default_position(new_position):
 #Functions that modify the player window
 def sizebox_toggle(window_class=player_class):
     """
-    Toggles whether the window matching the class has a border that allows for resizing
+    Toggles whether the window has a resizing border (Lets you resize by dragging the corner)
     """
     print("sizebox_toggle()")
+    #For using scroll lock to enable/disable the hotkeys
     if require_scroll and not GetKeyState(VK_SCROLL):
         return
-    print("VK_SCROLL SUCCESS")
     global size_box
     size_box = not size_box
-    multiplier = 2 * int(size_box) - 1 #Converts True to -1 and False to 1
-    style = user32.GetWindowLongPtrA(window_handle,win32con.GWL_STYLE) + ((multiplier) * win32con.WS_SIZEBOX)
-    user32.SetWindowLongPtrA(window_handle,win32con.GWL_STYLE,style)
+    multiplier = 2 * int(size_box) - 1 #Converts True to -1 and False to 1. This was an oversimplification.
+    style = user32.GetWindowLongPtrA(window_handle,win32con.GWL_STYLE) + ((multiplier) * win32con.WS_SIZEBOX) #Set style variable to current window style with sizebox toggled
+    user32.SetWindowLongPtrA(window_handle,win32con.GWL_STYLE,style) #sets window style flags to match the new style variable
     return
+
 
 
 def set_floating_window(window_class=player_class):
@@ -101,26 +117,25 @@ def set_floating_window(window_class=player_class):
     global window_handle
     global size_box
     global floating_height
-    print(f"window_handle: {window_handle}")
     window_handle = win32gui.FindWindowEx(None,None,window_class,None)
-    print(f"window_handle: {window_handle}")
+
+    #Switches the window to it's floating size.
     new_height = floating_height
     new_width = floating_width
     new_posy = floating_posy
 
+    #this tries to get the x and y coordinates by asking mpv itself, but that functionality was broken when I stopped working on the project.
+    #Instead it uses the display resolution from windows api and does some weird math with magic numbers that I no longer understand.
     if not mpv_error:
         try:
             #TODO: FIX
-            mpv = MPV(start_mpv=False, ipc_socket="mpvsocket")
+            #mpv = MPV(start_mpv=False, ipc_socket="mpvsocket")
             #width = mpv.command("get_property","width")
             #height = mpv.command("get_property","height")
             
-
+            
             width = user32.GetSystemMetrics(0)
             height = user32.GetSystemMetrics(1)
-            print(f"width: {width}")
-            print(f"height: {height}")
-            print(f"floating_height: {floating_height}")
             new_width = round(width/height * floating_height)
             if round(width/height*3) == 4:
                 new_posy -= 30
@@ -134,7 +149,7 @@ def set_floating_window(window_class=player_class):
     
     #TODO: FIX
     user32.SetWindowPos(window_handle,ctypes.wintypes.HWND(win32con.HWND_TOPMOST),floating_posx,new_posy,new_width,new_height,0)
-    user32.SetWindowLongPtrA(window_handle,win32con.GWL_STYLE,335740928) #335740928 corresponds to a window which without 0x800000, 0x400000, 0x80000, or 0x40000
+    user32.SetWindowLongPtrA(window_handle,win32con.GWL_STYLE,335740928) #335740928 corresponds to a window without 0x800000, 0x400000, 0x80000, or 0x40000. Those are window style flags by the way.
     size_box = False
     return
 
@@ -174,7 +189,8 @@ def reset_window(window_class=player_class):
 
 def toggle_autoplay():
     """
-    Toggles whether the video video player automatically plays mpv
+    Toggles whether the video player automatically plays the next video in folder.
+    This function is a mess.
     """
     global use_logitech_lighting
     if require_scroll and not GetKeyState(VK_SCROLL):
@@ -210,7 +226,9 @@ def toggle_autoplay():
 
 def autoplay_lighting(autoplay):
     """
-    Set lighting for F4 key based on whether next episodes will autoplay
+    This function I wrote for myself back when I made this program, it causes it to send commands to a separate python script I made to control the Logitech lighting SDK. (called 'LogiPy')
+    Honestly this isn't really useful to anyone.
+    Set lighting for F4 key based on whether next episodes will autoplay.
     """
     global use_logitech_lighting
     if not use_logitech_lighting:
@@ -220,6 +238,7 @@ def autoplay_lighting(autoplay):
     if require_scroll and not GetKeyState(VK_SCROLL):
         return
 
+    #This was my first attempt at inter-process communication.
     address = ('localhost', 6001)
     try:
         conn = Client(address, authkey=b'LogiLed')
@@ -255,6 +274,7 @@ atexit.register(exit_handler)
 
 
 if __name__ == "__main__":
+    #This is where you can adjust hotkeys.
     keyboard.add_hotkey("F1",lambda: sizebox_toggle())
     keyboard.add_hotkey("F2",lambda: set_floating_window())
     keyboard.add_hotkey("F3",lambda: reset_window())
